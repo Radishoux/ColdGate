2de2183a1ca95d48e1202d8780d3b3f01153893d
Rudy <rudy.quinternet@gmail.com>
2021-04-19 22:38:05 +0200

    add amp

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
+import Amplify from "aws-amplify";
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
