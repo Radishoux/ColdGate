4bd70b4957204e70d9b0d689105cef053829cabb
Rudy <rudy.quinternet@gmail.com>
2021-04-20 15:46:39 +0200

    env acc

--- cypress/integration/sendmsg.js
+++ cypress/integration/sendmsg.js
@@ -4,5 +4,6 @@ describe('login', () => {
     it('login', () => {
         cy.visit(env.prodlink)
         cy.get('#msg', { timeout: 100000 }).type(env.test_text1).type('{enter}')
+        // cy.get('#username', { timeout: 100000 }).type(env.test_text1).type('{enter}')
     })
 })
