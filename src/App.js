af009a86ca6b1e4135239a96d6762340856b2eaa
Pierre-Francois <pfalbo1@gmail.com>
2021-04-21 11:02:32 +0200

    add workshop socket io

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { Typography } from 'antd';
 import menus from "./logo/menu.png"
 import arrow from "./logo/left-arrow.png"
 import user from "./logo/user.png"
+import send from "./logo/send.png"
 
 import './App.css';
 
@@ -20,6 +21,7 @@ function App() {
     const [sideVisibiliy, setSideVisibiliy] = useState(6);
     const [buttonVis, setbuttonVis] = useState("none");
     const [conversations, setConv] = useState(["user1", "user2", "user3", "user4", "user5", "user6", "user7", "user8", "user9", "user10", "user11", "user12"]);
+    // const [conversations, setConv] = useState(["user1", "user2", "user3"]);
     const [AllMess, setMessage] = useState([{ user: 1, message: "Hello there" }, { user: 1, message: "How are you?" }, { user: 0, message: "Fine thx" }, { user: 0, message: "and you ?" }, { user: 0, message: "yup" }])
 
     function changeSide() {
@@ -41,7 +43,16 @@ function App() {
     }
     function addMess(e) {
         if (e.key === 'Enter') {
-            setMessage(state => [...state, { user: 0, message: e.target.value }]);
+            if (e.target.value != "") {
+                setMessage(state => [...state, { user: 0, message: e.target.value }]);
+                setTimeout(() => { document.getElementById("msg").value = ""; }, 100);
+            }
+        }
+    }
+    function addMessbtn() {
+        if (document.getElementById("msg").value != "") {
+            setMessage(state => [...state, { user: 0, message: document.getElementById("msg").value }]);
+            setTimeout(() => { document.getElementById("msg").value = ""; }, 100);
         }
     }
 
@@ -53,10 +64,9 @@ function App() {
                         <img className="userImg" src={user}></img>
                     </figure>
                     <div>
-                        <Title style={{ color: "white" }} level={5}>{elem}</Title>
-                        <h5></h5>
+                        <Title style={{ color: "white", marginTop: "1rem", marginBottom: "0.7rem" }} level={5}>{elem}</Title>
 
-                        <p className="pUser" style={{ maxWidth: "100%" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                        <p className="pUser" style={{ maxWidth: "100%" }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit ipsum dolor Lorem consectetur.</p>
                     </div>
                 </li>
             )
@@ -93,7 +103,7 @@ function App() {
                 <Row>
                     <Col xs={2} className="one">
                         {/* <button onClick={() => tests()}></button> */}
-                        <img src={menus} onClick={changeSide} style={{ width: "3vh", marginTop: "1vh", float: "left", marginLeft: "1.5vh", display: buttonVis }}></img>
+                        <img src={menus} onClick={changeSide} style={{ width: "3vh", marginTop: "1vh", float: "left", marginLeft: "1.5vh", display: buttonVis, cursor: "pointer" }}></img>
 
                     </Col>
 
@@ -103,7 +113,7 @@ function App() {
                             <Title style={{ marginTop: "1vh", float: "left", marginLeft: "5vh", color: "white" }} level={3}>Chats</Title>
                             <h3 ></h3>
 
-                            <img src={arrow} onClick={changeSide} style={{ float: "right", width: "3vh", marginTop: "1vh" }}></img>
+                            <img src={arrow} onClick={changeSide} style={{ float: "right", width: "3vh", marginTop: "1vh", cursor: "pointer" }}></img>
                         </div>
 
 
@@ -123,9 +133,10 @@ function App() {
                 </Row>
                 <div className="footerMess">
                     <div className="form__group field">
-                        <input autoComplete="off" onKeyDown={addMess} type="input" className="form__field" placeholder="Msg" name="msg" id='msg' required />
+                        <input type="reset" autoComplete="off" onKeyDown={addMess} type="input" className="form__field" placeholder="Msg" name="msg" id='msg' required />
                         <label htmlFor="name" className="form__label">Message</label>
                     </div>
+                    <img src={send} onClick={addMessbtn} style={{ marginLeft: "1.5rem", width: "4vh", marginTop: "4vh", cursor: "pointer" }}></img>
                 </div>
 
             </div>
