8e04f8803e2d8057adfef8076d6ff89c7af14e30
Rudy <rudy.quinternet@gmail.com>
2021-05-19 12:12:18 +0200

    push

---
+++ build/static/js/20.11355695.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpcoldgate=this.webpackJsonpcoldgate||[]).push([[20],{295:function(t,e,n){"use strict";n.r(e),n.d(e,"amplify_container",(function(){return i}));var c=n(21),i=function(){function t(t){Object(c.k)(this,t)}return t.prototype.render=function(){return Object(c.i)(c.b,null,Object(c.i)("slot",null))},t}();i.style=":host{display:-ms-flexbox;display:flex;-ms-flex-align:center;align-items:center;-ms-flex-pack:center;justify-content:center;height:100vh}"}}]);
+//# sourceMappingURL=20.11355695.chunk.js.map
